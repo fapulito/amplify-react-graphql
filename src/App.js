8b15d0f647de5b2b3a62b7b576871baf21d73cb4
fapulito <fapulito@gmail.com>
2023-09-02 07:38:17 -0700

    changes for update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Click for California Vision!
+          Updated from Local Terminal with GitHub!
         </a>
       </header>
     </div>
